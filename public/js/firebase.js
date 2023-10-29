c8c16983d7aaf6839da8c75cf0543fe828c4c65d
SANJU DAS <94630907+sd7918@users.noreply.github.com>
2023-10-29 23:38:04 +0530

    all files upload

---
+++ public/js/firebase.js
@@ -0,0 +1,16 @@
+
+let firebaseConfig = {
+    apiKey: "AIzaSyAMqbbN1Vcin19Eeo-dW4KD2I3AzOnqikQ",
+    authDomain: "blogging-website-65dbc.firebaseapp.com",
+    projectId: "blogging-website-65dbc",
+    storageBucket: "blogging-website-65dbc.appspot.com",
+    messagingSenderId: "1048342874983",
+    appId: "1:1048342874983:web:391d77857925b9a02afdc2"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+let db=firebase.firestore();
+
+
+
